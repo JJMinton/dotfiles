d31187f424591ac5ae719215b9054a9303f18f33
Jeremy Minton <jjm61@cam.ac.uk>
2015-09-11 10:14:38 +0100

    Added initial vimrc files

---
+++ vimrc/generate_vimrc.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Run this file to append .vimrc or copy the contents of this folder to vim/plugins
+echo "source ~/dotfiles/vimrc/vimrc_colors" >> .vimrc
+echo "source ~/dotfiles/vimrc/vimrc_tabs" >> .vimrc
+echo "source ~/dotfiles/vimrc/vimrc_arrows" >> .vimrc
+echo "source ~/dotfiles/vimrc/vimrc_beeps" >> .vimrc
+echo "source ~/dotfiles/vimrc/vimrc_numbering" >> .vimrc
