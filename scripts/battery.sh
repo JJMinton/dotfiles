a62981add6f939748e78d1f8a0dba4cdfb22200d
JJMinton <jeremyminton@gmail.com>
2016-10-04 00:02:31 +0100

    Refined xmobar scripts and added todo keys in vim

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-battery_list="Batt: "
+battery_list=""
 for dir in `find /sys/class/power_supply/ -name "BAT*"`; do
 	bat=`cat $dir/energy_now`
 	batcap=`cat $dir/energy_full`
 	batperc=$((100*bat/batcap))
-	battery_list="$battery_list $batperc,"
+	battery_list="$battery_list$batperc,"
 done
 batts=`echo $battery_list | sed 's/,$//'`
 echo $batts
