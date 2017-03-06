8f47786fe9911c475fb1d98e2b56d6a4e9d73fbb
JJMinton <jeremyminton@gmail.com>
2017-03-06 10:30:24 +0000

    fixed xmobar hidding on laptop (no toggles with Mod+b)

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,6 +1,9 @@
 import XMonad
-import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.DynamicLog
+import XMonad.Util.EZConfig(additionalKeysP)
+import XMonad.Layout.NoBorders(smartBorders)
+import XMonad.Hooks.ManageHelpers
 import XMonad.Util.Run
 import System.IO
 
@@ -14,17 +17,34 @@ import System.IO
 --Define workspaces names
 myWorkspaces = ["1:main","2:reference","3:output","4:misc","5:writing","6:output","7:reading","8:media","9:social"]
 
-main = do
-xmproc <- spawnPipe "/usr/bin/xmobar ~/dotfiles/xmonad/xmobarrc" --requires the installation of xmobar (sudo pacman -S xmobar)
-xmonad $ defaultConfig
-    {borderWidth=2,
-     layoutHook = avoidStruts $ layoutHook defaultConfig,
-     --layoutHook = myLayout,
-     workspaces = myWorkspaces,
-     manageHook = manageDocks <+> manageHook defaultConfig, --requires the installation of xmonad-contrib ? 
-     logHook = dynamicLogWithPP xmobarPP
-        { ppOutput = hPutStrLn xmproc,
-          ppTitle = xmobarColor "blue" "" . shorten 50,
-          ppLayout = const "" -- to disable the layout info on xmobar
-        }
-     }
+
+myConfig = defaultConfig
+    { manageHook = ( isFullscreen --> doFullFloat ) <+> manageDocks <+> manageHook defaultConfig
+    --, manageHook = manageDocks <+> manageHook defaultConfig --requires the installation of xmonad-contrib ? 
+    , layoutHook = smartBorders (avoidStruts $ layoutHook defaultConfig)
+    --, terminal = "urxvtcd"
+    , borderWidth=1
+    --, modMask = mod4Mask --sets mod key to windows button
+    --, layoutHook = myLayout
+    , workspaces = myWorkspaces
+    }
+
+-- The main function
+main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
+
+-- Launch the bar
+myBar = "xmobar ~/dotfiles/xmonad/xmobarrc" --requires the installation of xmobar (sudo pacman -S xmobar)
+
+-- Custom PP, configure it as you like. It determines what is being written ot the bar.
+myPP = xmobarPP { ppLayout = const "" -- to disable the layout infor on xmobar
+                --, ppCurrent = xmobarColor "green" "" . wrap "<" ">" . shorten 68
+                , ppTitle = xmobarColor "blue" "" . shorten 50
+                }
+--myPP = xmobarPP
+--        { ppOutput = hPutStrLn spawnPipe "/usr/bin/xmobar ~/dotfiles/xmonad/xmobarrc"
+--        , ppTitle = xmobarColor "blue" "" . shorten 50
+--        , ppLayout = const "" -- to disable the layout info on xmobar
+--        }
+
+-- Key binding to toggle the gap for the bar.
+toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
