67bd6b75c6fb6dfc56aaf9f804b2c95345a5dd1c
Jeremy Minton <jeremyminton@gmail.com>
2020-06-15 18:59:16 +0100

    i3 refinements

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-import -window root /home/jeremy.minton/Pictures/screenshot.png
-i3lock -i /home/jeremy.minton/Pictures/screenshot.png -p default -n
+#import -window root /home/jeremy.minton/Pictures/screenshot.png
+#i3lock -i /home/jeremy.minton/Pictures/screenshot.png -p win -n -e -c 000000
+i3lock -i /home/jeremy.minton/.wallpapers/.lockscreen/screenshot.png -p win -n -e -c 000000
