20307eecbd2391636059548ec4e023dbac667d6d
Jeremy Minton <jeremyminton@gmail.com>
2020-06-15 18:57:40 +0100

    Add user services start scripts

---
+++ scripts/services/outlook.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+#
+# outlook.sh
+# Copyright (C) 2018 jeremy-minton <jeremy.minton@eigentech.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+systemctl --user restart outlook
---
+++ scripts/services/outlookcalendar.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+#
+# outlookcalendar.sh
+# Copyright (C) 2018 jeremy-minton <jeremy.minton@eigentech.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+systemctl --user restart outlookcalendar
